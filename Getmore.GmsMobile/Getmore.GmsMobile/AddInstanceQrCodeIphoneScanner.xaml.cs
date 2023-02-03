84cfa716e13a432b84f58e8724c421e32128fa63
Danil Tkachenko <d_tkachenko@utb.cz>
2023-02-03 15:15:54 +0100

    scan fixed

--- Getmore.GmsMobile/Getmore.GmsMobile/AddInstanceQrCodeIphoneScanner.xaml.cs
+++ Getmore.GmsMobile/Getmore.GmsMobile/AddInstanceQrCodeIphoneScanner.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Getmore.GmsMobile.Core.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ using ZXing;
 namespace Getmore.GmsMobile
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AddInstanceQrCodeIphoneScanner 
+	public partial class AddInstanceQrCodeIphoneScanner
 	{
 		public AddInstanceQrCodeIphoneScanner()
 		{
@@ -22,9 +23,17 @@ namespace Getmore.GmsMobile
 			Device.BeginInvokeOnMainThread(async () =>
 			{
 				await DisplayAlert("Scanned result", result.Text, "OK");
+				//this.MyResult = new GmsInstanceLoginData(resultArr[0], resultArr[1], null, resultArr[2]);
+
 				_ = this.Navigation.PopModalAsync();
 
 			});
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			_ = this.Navigation.PopModalAsync();
+
 		}
 	}
 }
